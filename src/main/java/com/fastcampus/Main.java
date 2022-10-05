3f502bd513dcac2f1d83dfcdb7c64921c53e9282
leeuisu <leeuisu@naver.com>
2022-10-05 21:43:10 +0900

    Update #3 - Main
    #3 여기서 마무리

--- src/main/java/com/fastcampus/Main.java
+++ src/main/java/com/fastcampus/Main.java
@@ -1,12 +1,13 @@
 package com.fastcampus;
 
 import com.fastcampus.logic.BubbleSort;
+import com.fastcampus.logic.*;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
         System.out.println("[result]" + sort.sort(Arrays.asList(args)));
     }
 
